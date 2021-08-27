d0bb081419aba535b0a1f30a513d341e00f89de0
thisaramanohara <thisaramanohara@gmail.com>
2021-08-28 02:54:42 +0530

    Grid OK

--- js/game.js
+++ js/game.js
@@ -1,4 +1,10 @@
 function checkScore() {
-    var check = document.getElementById("xx").value;
-    alert(check);
+    var check = document.getElementById("31").value;
+    var score = 0;
+    if(check == 'C' || check == 'c' ){
+        alert(5);
+    }else{
+        alert(0);
+    }
+    
 }
