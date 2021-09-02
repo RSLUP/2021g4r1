1722e4319a49cff5982314b652d8f63b38570518
isu92neth <123csissa@gmail.com>
2021-09-02 08:36:33 +0530

    Add sample quiz content

---
+++ js/quizzes/quiz_content.js
@@ -0,0 +1,35 @@
+//array to pass question number, questions, options and correct answers
+
+let quiz_content = [
+  {
+    number: 1,
+    question: "Synonym of ARROGANT is ________?",
+    options: ["Conceited", "Humble", "Progressive", "Noble"],
+  },
+
+  {
+    number: 2,
+    question: "It’s difficult ______ reconcile such different points of view",
+    options: ["with", "to", "in", "on"],
+  },
+  {
+    number: 3,
+    question: 'Passive of "Why did you disobey your parents?"',
+    options: [
+      "Why do your parents was disobey by you?",
+      "Why were your parents disobeyed by you?",
+      "Why your parents was disobeyed by you?",
+      "Why your parents  were disobey by you?",
+    ],
+  },
+  {
+    number: 4,
+    question: "There wasn’t much unity ______ the council members",
+    options: ["Among", "From", "For", "None of the above"],
+  },
+  {
+    number: 5,
+    question: "Choose the word with correct spellings.",
+    options: ["Commission", "Commisson", "Commision", "Comission"],
+  },
+];
