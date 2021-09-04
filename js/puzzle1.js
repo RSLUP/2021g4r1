519ad97f46f755bd02fa13da66fce541a580601a
thisaramanohara <thisaramanohara@gmail.com>
2021-09-04 20:23:02 +0530

    Puzzle 1 score edited

--- js/puzzle1.js
+++ js/puzzle1.js
@@ -16,70 +16,85 @@ function checkScore() {
     var score = 0;
 
     var increment = 100 / 13;
+    var correct = 0;
     if(check14 == 'O' || check14 == 'o' ){
         score += increment;
+        correct++
         //alert(Math.ceil(score))
     }
 
     if(check16 == 'A' || check16 == 'a' ){
         score += increment;
+        correct++
         //alert(Math.ceil(score))
     }
 
     if(check31 == 'T' || check31 == 't' ){
         score += increment;
+        correct++
         //alert(Math.ceil(score))
     }
 
     if(check32 == 'E' || check32 == 'e' ){
         score += increment;
+        correct++
         //alert(Math.ceil(score))
     }
 
     if(check35 == 'I' || check35 == 'i' ){
         score += increment;
+        correct++
         //alert(Math.ceil(score))
     }
 
     if(check36 == 'F' || check36 == 'f' ){
         score += increment;
+        correct++
         //alert(Math.ceil(score))
     }
 
     if(check45 == 'L' || check45 == 'l' ){
         score += increment;
+        correct++
         //alert(Math.ceil(score))
     }
 
     if(check53 == 'A' || check53 == 'a' ){
         score += increment;
+        correct++
         //alert(Math.ceil(score))
     }
 
     if(check55 == 'A' || check55 == 'a' ){
         score += increment;
+        correct++
         //alert(Math.ceil(score))
     }
 
     if(check61 == 'A' || check61 == 'a' ){
         score += increment;
+        correct++
         //alert(Math.ceil(score))
     }
     if(check64 == 'E' || check64 == 'e' ){
         score += increment;
+        correct++
         //alert(Math.ceil(score))
     }
     if(check67 == 'O' || check67 == 'o' ){
         score += increment;
+        correct++
         //alert(Math.ceil(score))
     }
     if(check76 == 'O' || check76 == 'o' ){
         score += increment;
+        correct++
         //alert(Math.ceil(score))
     }
 
     //alert(score)
     //alert(Math.ceil(score))
-    document.getElementById("score").innerHTML = Math.ceil(score);
+    // document.getElementById("score").innerHTML = Math.ceil(score);
+    document.getElementById("score").innerHTML = correct + "/13";
 
 }
